7ab693d49918c9da2a6380da32d00ce2aec4ac00
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-09 21:29:07 +0300

    finishing google and github oAuth

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
 
 const Nav = async () => {
+  const session = await getServerSession(options);
   return (
     <header className="bg-gray-600 text-gray-100">
       <nav className="flex justify-between items-center w-full px-10 py-4">
@@ -11,12 +14,11 @@ const Nav = async () => {
           <Link href="/ClientMember">Client Member</Link>
           <Link href="/Member">Member</Link>
           <Link href="/Public">Public</Link>
-          <Link href="/api/auth/signin">Login</Link>
-          {/* {session ? (
+          {session ? (
             <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
           ) : (
             <Link href="/api/auth/signin">Login</Link>
-          )} */}
+          )}
         </div>
       </nav>
     </header>
---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,92 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "../../../(model)/User";
+import bcrypt from "bcrypt";
+
+export const options = {
+  providers: [
+    GitHubProvider({
+      profile(profile) {
+        console.log("Profile GitHub: ", profile);
+
+        let userRole = "GitHub User";
+        if (profile?.email == "tiledan2015@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      profile(profile) {
+        console.log("Profile Google: ", profile);
+
+        let userRole = "Google User";
+        return {
+          ...profile,
+          id: profile.sub,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     email: {
+    //       label: "email:",
+    //       type: "text",
+    //       placeholder: "your-email",
+    //     },
+    //     password: {
+    //       label: "password:",
+    //       type: "password",
+    //       placeholder: "your-password",
+    //     },
+    //   },
+    //   async authorize(credentials) {
+    //     try {
+    //       const foundUser = await User.findOne({ email: credentials.email })
+    //         .lean()
+    //         .exec();
+
+    //       if (foundUser) {
+    //         console.log("User Exists");
+    //         const match = await bcrypt.compare(
+    //           credentials.password,
+    //           foundUser.password
+    //         );
+
+    //         if (match) {
+    //           console.log("Good Pass");
+    //           delete foundUser.password;
+
+    //           foundUser["role"] = "Unverified Email";
+    //           return foundUser;
+    //         }
+    //       }
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //     return null;
+    //   },
+    // }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
